799b6bc0c2d16fef18ff5773e9e107d948b2d99b
manaoka <manaoka@m.com>
2025-01-07 09:06:36 +0900

    初回コミット　メインページのsp完了、pc版途中

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hanacafe Nappa69</title>
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Hammersmith+One&family=Kiwi+Maru&family=Noto+Sans+JP:wght@100..900&display=swap"
+        rel="stylesheet">
+    <!-- CSS  -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- JavaScript -->
+    <script src="/js/iquery.js"></script>
+    <script src="/js/script.js" defer></script>
+    <script src="/js/slick.min.js"></script>
+</head>
+
+<body>
+    <!-- header -->
+    <header id="nav" class="header-nav">
+
+        <h1 class="header-logo" href="#">
+            <img src="img/hanacafe-icon-white.svg" alt="Hana Cafe Nappa69">
+        </h1>
+
+        <!-- ハンバーガーメニュー -->
+        <button class="btn-gNav">
+            <span class="btn-gNav__line"></span>
+        </button>
+
+        <nav>
+            <ul class="gnav-list">
+                <li><a class="nav-hover-style" href="#">Home</a></li>
+                <li><a class="nav-hover-style" href="#consept">Concept</a></li>
+                <li>
+                    <a class="nav-hover-style" href="#">Menu</a>
+                    <ul class="sub-list">
+                        <li><a class="nav-hover-style" href="#">Lunch</a></li>
+                        <li><a class="nav-hover-style" href="#">Cafe</a></li>
+                        <li><a class="nav-hover-style" href="#">Drink</a></li>
+                        <li><a class="nav-hover-style" href="#">Dinner</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-hover-style" href="#">News</a></li>
+                <li><a class="nav-hover-style" href="#access">Access</a></li>
+                <li><a class="btn" href="https://retty.me/reserve/100000067832/" target="_blank" rel="noopener noreferrer">ご予約</a></li>
+
+            </ul>
+        </nav>
+    </header>
+
+    <main id="home">
+        <!-- main visual -->
+        <div class="mv">
+            <div class="mv-title">
+                <img src="/img/h1-hanacafe.png" alt="Hana Cafe">
+                <img src="/img/h1-nappa.png" alt="Nappa69">
+            </div>
+        </div>
+        <div class="scroll-area">
+            <a class="scroll" href="#consept">
+                scrool
+                <span></span>
+            </a>
+        </div>
+
+        <!-- consept -->
+        <section id="consept" class="consept-area">
+            <h2 class="title-concept">
+                <img src="/img/title-concept.png" alt="Concept">
+            </h2>
+            <div class="consept-content">
+                <p>
+                    細い路地を抜けた先にある、<br>
+                    花と植物いっぱいのカフェ<br>
+                    店先では季節の花鉢が<br class="hidden-pc">静かにお出迎え。<br>
+                    棚には不思議なかたちの<br>
+                    多肉植物やサボテン達<br>
+                </p>
+                <div class="concept-photo-decoration-1">
+                    <img src="/img/concept-1.png" alt="店の看板の写真">
+                </div>
+                <p>
+                    お料理には玄米や有機野菜、<br>
+                    旬の地産野菜を中心に、<br>
+                    体に優しい心も嬉しいメニューを<br>
+                    ご用意しております。<br>
+                </p>
+                <div class="concept-photo-decoration-2">
+                    <img src="/img/concept-2.png" alt="海鮮パスタの写真">
+                </div>
+                <p>
+                    手塗りの壁に<br>
+                    木のぬくもりを感じる<br class="hidden-pc">椅子とテーブル、<br>
+                    ゆっくりくつろぎたい方には<br>
+                    ソファー席をどうぞ。<br>
+                    ひとりの時間を大切にできる<br>
+                    カウンター席も多数ございます。<br>
+                </p>
+                <div class="concept-photo-decoration-3">
+                    <img src="/img/concept-3.png" alt="店内のソファー席の写真">
+                </div>
+                <p>
+                    住宅街にひっそりとたたずむ<br class="hidden-pc">古民家カフェで、<br>
+                    ゆっくりとした時間を<br class="hidden-pc">過ごしてみませんか。<br>
+                </p>
+            </div>
+        </section>
+
+        <!-- menu -->
+        <section id="menu" class="menu-area">
+            <h2 class="title-menu">
+                <img src="/img/title-menu.png" alt="Menu">
+            </h2>
+            <div class="MenuSlider">
+                <div><img src="/img/menu-1.png" alt="プリン"></div>
+                <div><img src="/img/menu-2.png" alt="ハンバーグ"></div>
+                <div><img src="/img/menu-3.png" alt="ケーキ"></div>
+                <div><img src="/img/menu-4.png" alt="カレーライス"></div>
+                <div><img src="/img/menu-5.png" alt="カプレーゼ"></div>
+                <div><img src="/img/menu-6.png" alt="和食プレート"></div>
+                <div><img src="/img/menu-7.png" alt="パスタ"></div>
+                <div><img src="/img/menu-8.png" alt="パフェ"></div>
+                <div><img src="/img/menu-9.png" alt="アヒージョ"></div>
+            </div>
+            <a class="view-more" href="#menu"><img src="/img/arrow.svg" alt="view more"></a>
+        </section>
+
+        <!-- news -->
+        <section id="news" class="news-area">
+            <h2 class="title-news">
+                <img src="/img/title-news.png" alt="News">
+            </h2>
+            <ul class="news-list">
+                <li>
+                    <a href="#">
+                        <time>2024年11月10日</time>
+                        <p class="news-list-title">
+                            12月営業日のお知らせ
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <time>2024年11月03日</time>
+                        <p class="news-list-title">
+                            Winter Coupon 配信開始！
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <time>2024年10月10日</time>
+                        <p class="news-list-title">
+                            11月営業日のお知らせ
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <time>2024年09月30日</time>
+                        <p class="news-list-title">
+                            Halloween<br class="hidden-pc">
+                            限定メニューのお知らせ
+                        </p>
+                    </a>
+                </li>
+            </ul>
+            <a class="list-display" href="#news"><img src="/img/news-arrow.svg" alt="お知らせ一覧"></a>
+        </section>
+
+        <!-- access -->
+        <section id="access" class="access-area">
+            <h2 class="title-access">
+                <img src="/img/title-access.png" alt="Access">
+                <div class="access-car" id="car"><img src="/img/access-car.gif" alt="car animetion"></div>
+            </h2>
+
+                <h3>HanaCAFE Nappa69</h3>
+                <address>
+                    <a href="tel:044-872-9288">044-872-9288</a>
+                    神奈川県川崎市中原区新丸子東1-983-1
+                </address>
+                <p>
+                    東急東横線「新丸子駅」東口より徒歩2分<br>
+                    ＪＲ南武線「武蔵小杉駅」北口より徒歩5分<br>
+                </p>
+                <p>
+                    伏見稲荷神社の北側の小さな小道をお入りください。
+                </p>
+                <img class="access-photo" src="/img/map-path.png" alt="伏見稲荷神社の北側の小さな小道">
+                <!--GoogleMap-->
+                <div class="access-map-area">
+                    <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3244.9690555848097!2d139.6621708148758!3d35.57915558644919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f5217aebdd87%3A0xe2f92c648a5718a2!2z5pel5pys44CB44CSMjExLTAwMDQg56We5aWI5bed55yM5bed5bSO5biC5Lit5Y6f5Yy65paw5Li45a2Q5p2x77yR5LiB55uu77yZ77yY77yT4oiS77yR!5e0!3m2!1sja!2sus!4v1735915932787!5m2!1sja!2sus"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <!--/GoogleMap-->
+                </div>
+        </section>
+    </main>
+    
+    <!-- footer -->
+    <footer id="footer">
+        <img class="footer-logo" src="/img/hanacafe-icon-white.svg" alt="Hana Cafe Nappa69">
+        <table>
+            <caption>営業時間</caption>
+            <tr>
+                <th>店休日</th>
+                <td>なし</td>
+            </tr>
+            <tr>
+                <th>Open</th>
+                <td> 11:00〜23:00</td>
+            </tr>
+            <tr>
+                <th>※月、火曜日</th>
+                <td>17:00 Colse</td>
+            </tr>
+            <tr>
+                <th>Lunch Time</th>
+                <td> 11:30〜16:00</td>
+            </tr>
+            <tr>
+                <th>Last Order </th>
+                <td>閉店30分前</td>
+            </tr>
+        </table>
+        <p class="takeout-text">
+            テイクアウト、デリバリーも承っております。
+        </p>
+        <a class="footer-btn" href="https://retty.me/reserve/100000067832/" target="_blank" rel="noopener noreferrer">
+            ご予約はこちら
+        </a>
+        <p>
+            その他お問い合わせ
+        </p>
+        <address>
+            <a href="tel:044-872-9288">044-872-9288</a>
+        </address>
+        <div>
+            <a href="#">
+                <img src="/img/instagram_icon.svg" alt="instagram">
+            </a>
+            <a href="#">
+                <img src="/img/X_icon.svg" alt="X">
+            </a>
+        </div>
+        <p class="copyright">
+            <small>&copy; HanaCafe Nappa69</small>
+        </p>
+    </footer>
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,130 @@
+
+
+// navの色反転============================
+
+jQuery(function ($) {
+    var nav = jQuery('#nav'); // ナビゲーション要素
+    var height = jQuery(window).height(); // ウィンドウの高さ
+    var w = $(window).width(); // ウィンドウの幅
+    var x = 960; // タブレットの画面幅
+    var y = 560; // スマホの画面幅
+    var footer = jQuery('#footer'); // フッター要素の追加
+    var footerTop = footer.offset().top; // フッターの上部位置を取得
+
+    function checkScroll() {
+        var scrollTop = jQuery(window).scrollTop(); // 現在のスクロール位置を取得
+
+        // デバッグ用にスクロール位置とフッター位置を出力
+        console.log('スクロール位置:', scrollTop); 
+        console.log('フッター位置:', footerTop);
+
+        if (w > x) { // 画面幅が961px以上のとき
+            if (scrollTop > height && scrollTop < footerTop) {
+                nav.addClass('invert'); // スクロールが画面の高さを越え、フッターに到達していない場合にクラスを追加
+            } else {
+                nav.removeClass('invert'); // それ以外の場合はクラスを削除
+            }
+        } else if (x >= w && w > y) { // 画面幅が561px以上960px以下のとき
+            if (scrollTop > 500 && scrollTop < footerTop) {
+                nav.addClass('invert'); // スクロールが500pxを越え、フッターに到達していない場合にクラスを追加
+            } else {
+                nav.removeClass('invert'); // それ以外の場合はクラスを削除
+            }
+        } else { // 画面幅が560px以下のとき
+            if (scrollTop > 250 && scrollTop < footerTop) {
+                nav.addClass('invert'); // スクロールが250pxを越え、フッターに到達していない場合にクラスを追加
+            } else {
+                nav.removeClass('invert'); // それ以外の場合はクラスを削除
+            }
+        }
+    }
+
+    jQuery(window).scroll(checkScroll); // スクロールイベントに関数をバインド
+    jQuery(window).resize(function () { // ウィンドウリサイズ時にフッターの位置を再取得
+        w = $(window).width(); // ウィンドウの幅を再取得
+        footerTop = footer.offset().top; // フッターの上部位置を再取得
+        checkScroll(); // 初期のスクロール状態をチェック
+    });
+
+    checkScroll(); // ページ読み込み時に初期状態をチェック
+});
+
+
+// ハンバーガーメニュー ============================
+$(function () {
+	$('.btn-gNav').on('click', function () {        // js-btnクラスをクリックすると、
+	  $(' nav , .btn-gNav').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+	})
+  });
+
+//   ページ内リンククリックで閉じる
+$(function () {
+    // ハンバーガーボタンクリックで実行
+    $('.nav-hover-style, .gnav-list .btn').click(function () {
+      $(this).toggleClass('open');
+      $(' nav , .btn-gNav').toggleClass('open');
+    });
+  
+    $('.drawer__nav__link').click(function () {
+      $(' nav , .btn-gNav').removeClass('open');
+      $(' nav , .btn-gNav').removeClass('open');
+    });
+    // function
+  });
+
+
+
+
+// MenuSlider ============================
+$(function () {
+	$('.MenuSlider').slick({
+		autoplay: true, // 自動でスクロール
+		autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+		speed: 10000, // スライドが流れる速度を設定
+		cssEase: "linear", // スライドの流れ方を等速に設定
+		slidesToShow: 9, // 表示するスライドの数
+		swipe: false, // 操作による切り替えはさせない
+		arrows: false, // 矢印非表示
+		pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+		pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+		responsive: [
+			{
+				breakpoint: 5000,
+				settings: {
+					slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+				}
+			}
+		]
+	});
+});
+
+
+
+// car
+
+/* 
+
+const image = document.getElementById('car'); // ロゴ画像
+
+// 画像を左端から右に動かす
+image.animate(
+	// 途中の状態を表す配列
+	[
+		{ transform: 'translate(40%, -40%)'}, // 開始時の状態（左端）
+		{ transform: 'translate(250%, -40%)' } // 終了時の状態（左端から--の位置）
+	], 
+	// タイミングに関する設定
+	{
+		fill: 'backwards', // 再生前後の状態（再生前、開始時の状態を適用）
+		duration: 6500, // 再生時間（1000ミリ秒）
+		iterations: Infinity,  // アニメーションの繰り返し回数（ずっと繰り返す）
+		delay: 1000, // 再生までの待機時間（1000ミリ秒）
+		easing: 'cubic-bezier(0.5, 0, 0.5, 1)', // 最初に加速するように設定
+	},
+);
+
+ */
+
+
+
+
