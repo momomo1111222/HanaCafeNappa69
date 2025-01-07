7b3dab9d741c2771e7141996971b382470a94564
manaoka <manaoka@m.com>
2025-01-07 13:34:02 +0900

    html書き換え

---
+++ menu.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hanacafe Nappa69</title>
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Hammersmith+One&family=Kiwi+Maru&family=Noto+Sans+JP:wght@100..900&display=swap"
+        rel="stylesheet">
+    <!-- CSS  -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- JavaScript -->
+    <script src="/js/iquery.js"></script>
+    <script src="/js/script.js" defer></script>
+    <script src="/js/slick.min.js"></script>
+</head>
+
+<body>
+    <!-- header -->
+    <header id="nav" class="header-nav">
+
+        <h1 class="header-logo" href="#">
+            <img src="img/hanacafe-icon-white.svg" alt="Hana Cafe Nappa69">
+        </h1>
+
+        <!-- ハンバーガーメニュー -->
+        <button class="btn-gNav">
+            <span class="btn-gNav__line"></span>
+        </button>
+
+        <nav>
+            <ul class="gnav-list">
+                <li><a class="nav-hover-style" href="#">Home</a></li>
+                <li><a class="nav-hover-style" href="#consept">Concept</a></li>
+                <li>
+                    <a class="nav-hover-style" href="#">Menu</a>
+                    <ul class="sub-list">
+                        <li><a class="nav-hover-style" href="#">Lunch</a></li>
+                        <li><a class="nav-hover-style" href="#">Cafe</a></li>
+                        <li><a class="nav-hover-style" href="#">Drink</a></li>
+                        <li><a class="nav-hover-style" href="#">Dinner</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-hover-style" href="#">News</a></li>
+                <li><a class="nav-hover-style" href="#access">Access</a></li>
+                <li><a class="btn" href="https://retty.me/reserve/100000067832/" target="_blank" rel="noopener noreferrer">ご予約</a></li>
+
+            </ul>
+        </nav>
+    </header>
+
+<!-- ======================== -->
+<!-- menu -->
+<!-- ======================== -->
+    <main>
+    <section id="menu" class="menu-area">
+            <h2 class="title-menu">
+                <img src="/img/title-menu.png" alt="Menu">
+            </h2>
+
+           
+        </section>
+
+
+
+
+
+
+
+        <!-- access -->
+        <section id="access" class="access-area">
+            <h2 class="title-access">
+                <img src="/img/title-access.png" alt="Access">
+                <div class="access-car" id="car"><img src="/img/access-car.gif" alt="car animetion"></div>
+            </h2>
+
+            <h3>HanaCAFE Nappa69</h3>
+            <address>
+                <a href="tel:044-872-9288">044-872-9288</a>
+                神奈川県川崎市中原区新丸子東1-983-1
+            </address>
+            <p>
+                東急東横線「新丸子駅」東口より徒歩2分<br>
+                ＪＲ南武線「武蔵小杉駅」北口より徒歩5分<br>
+            </p>
+            <p>
+                伏見稲荷神社の北側の小さな小道をお入りください。
+            </p>
+            <img class="access-photo" src="/img/map-path.png" alt="伏見稲荷神社の北側の小さな小道">
+            <!--GoogleMap-->
+            <div class="access-map-area">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3244.9690555848097!2d139.6621708148758!3d35.57915558644919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f5217aebdd87%3A0xe2f92c648a5718a2!2z5pel5pys44CB44CSMjExLTAwMDQg56We5aWI5bed55yM5bed5bSO5biC5Lit5Y6f5Yy65paw5Li45a2Q5p2x77yR5LiB55uu77yZ77yY77yT4oiS77yR!5e0!3m2!1sja!2sus!4v1735915932787!5m2!1sja!2sus"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <!--/GoogleMap-->
+            </div>
+        </section>
+    </main>
+
+    <!-- footer -->
+    <footer id="footer">
+        <img class="footer-logo" src="/img/hanacafe-icon-white.svg" alt="Hana Cafe Nappa69">
+
+        <table>
+            <caption>営業時間</caption>
+            <tr>
+                <th>店休日</th>
+                <td>なし</td>
+            </tr>
+            <tr>
+                <th>Open</th>
+                <td> 11:00〜23:00</td>
+            </tr>
+            <tr>
+                <th>※月、火曜日</th>
+                <td>17:00 Colse</td>
+            </tr>
+            <tr>
+                <th>Lunch Time</th>
+                <td> 11:30〜16:00</td>
+            </tr>
+            <tr>
+                <th>Last Order </th>
+                <td>閉店30分前</td>
+            </tr>
+        </table>
+        <div class="footer-btn-area">
+
+            <a class="footer-btn" href="https://retty.me/reserve/100000067832/" target="_blank" rel="noopener noreferrer">
+                ご予約はこちら
+            </a>
+            <p class="takeout-text">
+                テイクアウト、デリバリーも承っております。
+            </p>
+            <p>
+                その他お問い合わせ
+            </p>
+            <address>
+                <a href="tel:044-872-9288">044-872-9288</a>
+            </address>
+        </div>
+
+
+        <div class="sns-area">
+            <a href="#">
+                <img src="/img/instagram_icon.svg" alt="instagram">
+            </a>
+            <a href="#">
+                <img src="/img/X_icon.svg" alt="X">
+            </a>
+        </div>
+        <p class="copyright">
+            <small>&copy; HanaCafe Nappa69</small>
+        </p>
+    </footer>
+</body>
+
+</html>
