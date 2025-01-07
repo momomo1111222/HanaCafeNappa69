0d858cdac3d5ad34e243f7d8cc8269688be1d7b7
manaoka <manaoka@m.com>
2025-01-07 17:25:26 +0900

    ロード画面追加　メインページ完成

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+// loading画面============================
+
+//ローディング画面の処理
+
+/* /*  */
+window.addEventListener('DOMContentLoaded', function() {
+    const loadingElement = document.getElementById('loading');
+    
+    // ローディング画面を非表示にする関数
+    function hideLoading() {
+        loadingElement.style.transition = 'opacity 2s ease, visibility 1.5s ease';
+        loadingElement.style.opacity = 0;
+        loadingElement.style.visibility = 'hidden';
+    }
+    
+    // ページが初回訪問かどうかをセッションストレージで確認
+    if (!sessionStorage.getItem('firstVisit')) {
+        // 初回訪問の場合、セッションストレージにフラグをセット
+        sessionStorage.setItem('firstVisit', 'true');
+        
+        // ローディング画面を表示し、4秒後に非表示にする
+        setTimeout(function() {
+            hideLoading();
+        }, 3000);
+        
+    } else {
+        // 初回訪問ではない場合、ローディング画面を非表示にする
+    loadingElement.style.display = 'none';
+}
+});
+
+//*  */
+
+
+
 
 
 // navの色反転============================
